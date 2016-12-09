9967ddef9f7cf78d81a3275e05902354950fa4d6
Handong LEE <hansleo3589@gmail.com>
2016-12-09 13:20:27 +0900

    Add files via upload

---
+++ SQL_util.py
@@ -0,0 +1,30 @@
+#-*- coding:utf8 -*-
+import pymysql
+
+conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='3518', db='tweet_collector', charset='utf8')
+
+
+def select(data, option = ''):
+    curs = conn.cursor()
+
+    command = "SELECT " + str(data) + " FROM tweet "
+    if str(option) != '':
+        command += str(option)
+    command += ';'
+    print(command)
+    curs.execute(command)
+
+    return curs
+
+
+def insert(data):
+    curs = conn.cursor()
+    command = "INSERT INTO tweet(DateTime, tweet_id, Text)  VALUES( " + data + ' ) ;'
+    try:
+        curs.execute(command)
+        print('command', command)
+        conn.commit()
+    except:
+        print('error')
+        conn.rollback()
+
---
+++ twitt_listener.py
@@ -0,0 +1,80 @@
+# -*- coding: utf8 -*-
+
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import SQL_util
+import datetime
+
+ckey = 'lvkgpU4n1sQKvqb7OF707CnwV'
+csecret = 'BQbIIfzpRho1IzyDEpNTUtEXFMNh9rydUeJBsHqWHftxApJBgt'
+atoken = '713544464887775232-9HIAP4rIOSXmT44N2RnwuHECMMi6f6w'
+asecret = '2YNc3CYOesAwH9Y89hinjJ9fLFNMzzeGsLLaleVMhGmgn'
+keywords = ['니다.', '다.', '아요.', '한다.', '했다.', '있다.', '없다.', '다!', '까?']
+month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+
+class Listener(StreamListener):
+    def on_data(self, data):
+        print('\n----------------------------------------------------')
+        try:
+            data = data.encode("utf-8")
+            data = data.decode('unicode_escape')
+            for keyword in keywords:
+                if data.find(keyword) > 0:
+                    data = data.replace('{','')
+                    data = data.replace('}','')
+                    data_list = data.split(',"')
+                    a = 1
+                    insert_list = []
+
+                    for tweet in data_list:
+                        tweet_item = tweet.split('":')[-1]
+                        tweet_item = tweet_item.replace('"','')
+                        if a == 1:
+                            self.seperater_date(tweet_item)
+                            insert_list.append(self.seperater_date(tweet_item))
+                        elif a == 3:
+                            a += 1
+                            continue
+                        else:
+                            insert_list.append(tweet_item)
+                        a += 1
+                        if a == 5:
+                            break
+                    print(insert_list)
+
+                    insert_data = "'"+insert_list[0]+"', "+insert_list[1]+", '"+insert_list[2]+"'"
+                    print(insert_data)
+                    try:
+                        SQL_util.insert(insert_data)
+                        continue
+                    except:
+                        print('error')
+
+
+        except UnicodeEncodeError:
+            print("UnicodeEncodeError")
+        print('----------------------------------------------------\n')
+        return True
+
+    def on_error(self, status):
+        print('error : ', status)
+
+    def seperater_date(self, row_str):
+        date = '2016 '
+        date += row_str[4:16]
+        date = date.replace(' ','')
+        date_obj = datetime.datetime.strptime(date, '%Y%b%d%H:%M')
+        date_diff = date_obj + datetime.timedelta(hours=9)
+        result = ('%s/%s/%s' % (date_diff.year, date_diff.month, date_diff.day))
+        print(':',result, ':')
+        return result
+
+
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, Listener())
+twitterStream.filter(track=keywords)
+#twitterStream.filter(languages='ko')
